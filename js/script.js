2b1960496f8b5316f73613239f74c8742f742d4c
Vincent Tannos <tannos.vincent@gmail.com>
2024-09-04 22:55:34 +1000

    feat: add background and glitch effect
    Added a shaking effect when the panels are hovered over in discord mode,
    and added the new backgrounds

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 const themeToggleBtn = document.querySelector('.switch-mode');
 
 let flag = true;
+let intervalRet;
 
 themeToggleBtn.addEventListener('click', () => {
     document.body.classList.toggle('discord');
+    clearInterval(intervalRet);
+
     flag = !flag;
     if (flag == false) {
         document.getElementById("title").innerHTML = "Discord";
@@ -16,17 +19,39 @@ themeToggleBtn.addEventListener('click', () => {
         document.getElementById("name-5").innerHTML = "&ltmaybe it's my fault&gt - WILLOW";
         document.getElementById("name-6").innerHTML = "Raison Detre - Eve";
         document.getElementById("name-7").innerHTML = "Thnks fr th Mmrs - Fall Out Boy";
-        document.getElementById("name-8").innerHTML = "Who's Gonna Save U Now? - Rina Sawayama";        
+        document.getElementById("name-8").innerHTML = "Who's Gonna Save U Now? - Rina Sawayama";
+        
+        
+        function randomiseOffset() {
+            const panels = document.querySelectorAll(".panel");
+            panels.forEach((panel) => {
+                let xOffset = (Math.random() - Math.random()) * 40;
+                let yOffset = (Math.random() - Math.random()) * 40;
+                panel.style.setProperty("--x-offset", xOffset + "px");
+                panel.style.setProperty("--y-offset", yOffset + "px");
+            });
+        }
+        
+        intervalRet = setInterval(randomiseOffset, 80);
+        
     } else {
         document.getElementById("title").innerHTML = "Harmony";
 
         document.getElementById("name-1").innerHTML = "Evergreen - CIDER GIRL";
         document.getElementById("name-2").innerHTML = "Kesshou - Ryokuoushoku Shakai";
         document.getElementById("name-3").innerHTML = "Tek it - Cafuné";
-        document.getElementById("name-4").innerHTML = "Change your mind - The Killers";
+        document.getElementById("name-4").innerHTML = "Change Your Mind - The Killers";
         document.getElementById("name-5").innerHTML = "Jet Lag - Simple Plan ft Natasha Bedingfield";
         document.getElementById("name-6").innerHTML = "Koshaberi Biyori - FantasticYouth";
         document.getElementById("name-7").innerHTML = "Koi - Gen Hoshino";
         document.getElementById("name-8").innerHTML = "Talk - Beabadoobee";
+        
+        const panels = document.querySelectorAll(".panel");
+        panels.forEach((panel) => {
+            panel.style.setProperty("--x-offset", "0px");
+            panel.style.setProperty("--y-offset", "0px");
+        });
     }
 });
+
+
